ecb909f1ea112f3b90bbab6527f072aae82d0d99
sho20210126 <shohei210321@gmail.com>
2021-10-01 12:00:10 +0900

    モデル生成後

--- db/migrate/20210927133237_create_evaluations.rb
+++ db/migrate/20210927133237_create_evaluations.rb
@@ -1,7 +1,10 @@
 class CreateEvaluations < ActiveRecord::Migration[6.0]
   def change
     create_table :evaluations do |t|
-
+      t.integer :item_id
+      t.integer :user_id
+      t.integer :rate
+      t.string  :comment
       t.timestamps
     end
   end
--- db/migrate/20211001024722_users.rb
+++ db/migrate/20211001024722_users.rb
@@ -1,7 +1,7 @@
-class CreateUsers < ActiveRecord::Migration[6.0]
+class Users < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-
+      t.integer :age
       t.timestamps
     end
   end
